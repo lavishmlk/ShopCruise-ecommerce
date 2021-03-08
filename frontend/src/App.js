29e3e321a8df75d0e62d1962cb14566888371306
lavish <lavishmalik28@gmail.com>
2021-03-08 13:29:02 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>welcome to ShopCruise</h1>
+    </>
+  );
+
+  // function App() {
+  //   return (
+  //     <>
+  //       <h1>welcome to ShopCruise</h1>
+  //     </>
+  //   );
+};
+
+export default App;

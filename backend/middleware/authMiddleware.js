4cf4c7f1826a6633de4df4a6d6357a62323eb58e
lavishmlk <67894036+lavishmlk@users.noreply.github.com>
2021-06-29 11:51:59 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,4 @@
-//see middleware video of technical thappa first
-//Bassir Jafarzadeh
+
 
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
@@ -15,13 +14,10 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
 
-      //first parameter is always token and second is a secret
-      //ye mainly token ko decode karne ke liye
+     
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      //we dont want to send the password
-      //   So that is everything from the user document, minus the password.
-      //const user bhi kar sakte the par req property me dalna is good practice
+    
       req.user = await User.findById(decoded.id).select('-password');
 
       next();

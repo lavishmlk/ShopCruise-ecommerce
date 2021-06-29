e4e7d604a922b8ed4ae2c1f50d95c3bd83396222
lavishmlk <67894036+lavishmlk@users.noreply.github.com>
2021-06-29 11:55:56 +0530

    Update seeder.js

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,8 +1,3 @@
-//its a script we can run to import our data such as users,products
-//its no way connected to our server its completely seperate
-//see seeder video to understand all
-//... is spread operator iska use ye hai ki agar for rg ek array hai const array1=[lavish,malik,lmk] to ...array1 linkne se ye array ka sara data return kar dega
-//syntax  ...arrayname
 
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -24,21 +19,15 @@ const importData = async () => {
     await Product.deleteMany();
     await User.deleteMany();
 
-    //database me data add karne ke liye insertmany use karte hai
-    //for adding users to database ye users wo hai jo data folder me humne sample data dala hai
     const createdUsers = await User.insertMany(users);
     //now we want to make 1st user as admin
     const adminUser = await createdUsers[0]._id;
 
-    //for adding products to database jo humne data folder me banaya hai
-    //map react me sikha tha aur map ke aage jo product hai wo humne khud naam diya hai matlab as an argument to function
     const sampleProducts = products.map((product) => {
       //it means har product ko traverse karega jo product hai wo to aise ka aise hi rahega uske har har product me amdinUser wli field bhi add ho jaegi
       return { ...product, user: adminUser };
     });
 
-    //ye Product model hai
-    //isi line ki wajah se productmodel me humne data pass kiya
     await Product.insertMany(sampleProducts);
     console.log('data imported!');
     process.exit();

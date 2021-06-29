6bac2f028bcb0599d815b4fcc1fd1934716972a1
lavishmlk <67894036+lavishmlk@users.noreply.github.com>
2021-06-29 11:54:32 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,10 +11,7 @@ import { protect } from '../middleware/authMiddleware.js';
 //ALL THESE ARE ACTUALLY /API/USERS/ AND /API/USERS/LOGIN
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-//to implement middleware use it as first parameter
-//jab bhi iss route ko visit karenge middleware run karega
-//use router.route kyumki hume get req bhi chaahiye  aur profile update karwane ke liye put request bhi to get request hui ya put hui usi hissab se data pass hoga
-//aur ye get put requests axios ke through data lete time karte hai hum
+
 router
   .route('/profile')
   .get(protect, getUserProfile)

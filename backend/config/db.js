049c743061e26235951bf98ee759a6fae310657c
lavishmlk <67894036+lavishmlk@users.noreply.github.com>
2021-06-29 11:44:20 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-//mongoose hamesha promises return karta hai isliye async await use karna zaruri hai
+
 dotenv.config();
 const connectDB = async () => {
   try {
-    // process.env.MONGO_URI iska url .env wli file me likha hau isliye .env wli file bhi import karni padti hai
+   
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
